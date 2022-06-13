4d14d01b89cdd3bf5fb20ba4b41e8b90d23b7765
lizzy596 <lizmeek123@gmail.com>
2022-06-12 19:52:35 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect')
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const schema = require('./schema/schema')
+const port = process.env.PORT || 5000;
+
+
+
+//want to set graphiql to true when in development mode
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  }));
+
+
+
+
+  const start = async () => {
+    try {
+   
+     await connectDB(process.env.MONGO_URI)
+     app.listen(port, () => {
+       console.log(`Example app listening at http://localhost:${port}`)
+     })
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+   }
+   
+   
+   start() 
+
+
